5dd1640ca68b06006fbd7734a7cc84f1148fe537
raiddto <raiddto@gmail.com>
2023-06-13 17:00:59 +0800

    update

--- lib/flashcard.dart
+++ lib/flashcard.dart
@@ -4,13 +4,3 @@ class Flashcard {
 
   Flashcard({required this.question, required this.answer});
 }
-
-flashcard() {
-  List<Flashcard> _flashcards = [
-    Flashcard(question: "1", answer: "1"),
-    Flashcard(question: "2", answer: "2"),
-    Flashcard(question: "3", answer: "3"),
-    Flashcard(question: "4", answer: "4"),
-    Flashcard(question: "5", answer: "5"),
-  ];
-}
--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final List<Flashcard> _flashcards = [
+    Flashcard(question: "1", answer: "1"),
+    Flashcard(question: "2", answer: "2"),
+    Flashcard(question: "3", answer: "3"),
+    Flashcard(question: "4", answer: "4"),
+    Flashcard(question: "5", answer: "5"),
+  ];
+
   int _currIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('FlashCard Quiz')),
+        appBar: AppBar(title: const Text('FlashCard Quiz')),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -30,23 +38,21 @@ class _MyAppState extends State<MyApp> {
               width: 250,
               height: 250,
               child: FlipCard(
-                  front: FlashcardView(
-                      text: flashcard()._flashcards[_currIndex].question),
-                  back: FlashcardView(
-                      text: flashcard()._flashcards[_currIndex].answer)),
+                  front: FlashcardView(text: _flashcards[_currIndex].question),
+                  back: FlashcardView(text: _flashcards[_currIndex].answer)),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 OutlinedButton.icon(
                   onPressed: previousCard,
-                  icon: Icon(Icons.chevron_left),
-                  label: Text('Prev'),
+                  icon: const Icon(Icons.chevron_left),
+                  label: const Text('Prev'),
                 ),
                 OutlinedButton.icon(
                   onPressed: nextCard,
-                  icon: Icon(Icons.chevron_right),
-                  label: Text('Next'),
+                  icon: const Icon(Icons.chevron_right),
+                  label: const Text('Next'),
                 ),
               ],
             )
@@ -58,17 +64,14 @@ class _MyAppState extends State<MyApp> {
 
   void nextCard() {
     setState(() {
-      _currIndex = (_currIndex + 1 < flashcard()._flashcards.length)
-          ? _currIndex + 1
-          : 0;
+      _currIndex = (_currIndex + 1 < _flashcards.length) ? _currIndex + 1 : 0;
     });
   }
 
   void previousCard() {
     setState(() {
-      _currIndex = (_currIndex - 1 >= 0)
-          ? _currIndex - 1
-          : flashcard()._flashcards.length - 1;
+      _currIndex =
+          (_currIndex - 1 >= 0) ? _currIndex - 1 : _flashcards.length - 1;
     });
   }
 }
